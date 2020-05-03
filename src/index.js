47d2b8172a918d12761ca7719d001ee13efc32c8
Nishad TR <rimpoche@gmail.com>
2020-05-04 00:55:44 +0900

    passing district values to hotspot

--- src/index.js
+++ src/index.js
@@ -1269,7 +1269,7 @@ function setLang(lng) {
       }
 
       drawTrendChart(ddb.trend);
-      hotspotMap = drawHotspotMap(LANG);
+      hotspotMap = drawHotspotMap(ddb.prefectures, LANG);
 
       drawPrefectureTrajectoryChart(ddb.prefectures);
       drawGenderChart(ddb.gender);
@@ -1396,7 +1396,7 @@ window.onload = function () {
   // Set HTML language tag
   document.documentElement.setAttribute("lang", LANG);
   drawMap();
-  hotspotMap = drawHotspotMap(LANG);
+  hotspotMap = drawHotspotMap(ddb.prefectures, LANG);
 
   map.once("style.load", function (e) {
     styleLoaded = true;
