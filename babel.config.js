d2959b1afefe8847c07722767e951f4745e3c197
Akhil Balakrishnan <akhil.balakrishnan@citrix.com>
2020-10-11 15:24:50 +0530

    Improved performance - Load KPI data from separate file

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,18 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(false);
   return {
     plugins: [
-      "lodash"  // transforms/strips unused lodash deps.
+      "lodash", // transforms/strips unused lodash deps.
+      "@babel/transform-runtime",
     ],
     presets: [
       [
         "@babel/preset-env",
         {
           useBuiltIns: "entry",
-          targets: { chrome: "58", ie: "11" }
-        }
-      ]
-    ]
-  }
-}
+          targets: { chrome: "58", ie: "11" },
+        },
+      ],
+    ],
+  };
+};
